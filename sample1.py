533f9e785562501d33cb99a8209d81b491e607ac
Daniel Ezeh <Danielezeh3@gmail.com>
2024-03-10 06:47:43 -0400

    Added unit test file

---
+++ sample1.py
@@ -0,0 +1,15 @@
+'''
+this is the function
+'''
+
+def add(number1, number2):
+    '''
+    This function calculates the sume of two numbers
+    '''
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}.\n")
