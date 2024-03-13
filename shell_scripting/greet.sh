bd583d3c6e2e3f055eeb49529ec3042276902e57
Daniel Ezeh <Danielezeh3@gmail.com>
2024-03-13 10:04:11 -0400

    Added a folder and file for shell scripting

---
+++ shell_scripting/greet.sh
@@ -0,0 +1,7 @@
+# This script accepts the user\'s name and prints
+# a message greeting the user
+
+# Print the prompt message on screen
+echo -n "Enter your name: "
+
+# Wait for user to enter a name,
