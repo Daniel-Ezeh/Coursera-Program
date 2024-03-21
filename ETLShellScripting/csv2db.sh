31a00b61a5c9d9c2dc68a17eae18e5cb9d000f0b
Daniel-Ezeh <daniel.ezeh@nomba.com>
2024-03-21 10:00:35 -0400

    Updating script till transformation process

--- ETLShellScripting/csv2db.sh
+++ ETLShellScripting/csv2db.sh
@@ -11,3 +11,23 @@
 # Transforms the text delimiter from ":" to ",".
 
 # Loads the data from the CSV file into a table in PostgreSQL database
+
+
+echo "Extracting data"
+
+# Extract the columns 1 (user name), 2 (user id) and 
+# 6 (home directory path) from /etc/passwd
+
+cut -d ":" -f1,3,6 /etc/passwd > extracted-data.txt
+
+sleep 3
+
+# Transformation phase
+echo "Transforming data"
+
+# read the extracted data and replace the colons with comma
+ tr ":" "," < extracted-data.txt > transformed-data.csv
+ 
+
+
+
