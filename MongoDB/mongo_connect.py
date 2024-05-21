d92cfdbb82e4a7ab04311286bf4e87da9b9a4c40
Daniel-Ezeh <daniel.ezeh@nomba.com>
2024-05-21 11:36:48 -0400

    Added more codes

--- MongoDB/mongo_connect.py
+++ MongoDB/mongo_connect.py
@@ -14,6 +14,30 @@ for db in dbs:
     print(db)
 print("Closing the connection to the mongodb server")
 
+
+# Select the 'training' database
+db = connection.training
+
+# Select the 'python' collection
+collection = db.python
+
+#Creating a sample document
+doc = {
+    'lab':'Accessing mongodb using python',
+    'Subject':'No SQL Databases'
+}
+
+# Inserting a sample document
+print("Inserting a document into the \'python\' collection.")
+db.collection.insert_one(doc)
+
+# Querying for all documents in 'training' database and 'python' collection
+docs = db.collection.find()
+
+print("printing the documents in the collection.")
+for doc in docs:
+    print(doc)
+
 connection.close()
 
 
